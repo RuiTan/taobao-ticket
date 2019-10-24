408d2bbe819ea70c0d208e600c3a0515c0aa59be
I355050 <archer.tan@sap.com>
2019-10-24 18:23:01 +0800

    init

--- src/test/java/top/guitoubing/taobao/TaobaoApplicationTests.java
+++ src/test/java/top/guitoubing/taobao/TaobaoApplicationTests.java
@@ -1,13 +1,18 @@
 package top.guitoubing.taobao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaobaoApplicationTests {
 
+	private static Logger logger = LogManager.getLogger(TaobaoApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("test starts");
 	}
 
 }
